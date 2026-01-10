78ef95732f8a7aabaaf2d19b804a560fd2d349da
LookmanChitbahul <lookman1337cc@gmail.com>
2026-01-11 01:42:23 +0400

    removed dotenv line

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import {ENV} from './lib/env.js';
 import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
+import {ENV} from './lib/env.js';
 
 const app = express();
 const __dirname = path.resolve();
